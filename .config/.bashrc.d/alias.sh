32f12212de7066aaf7aa78baa7e6298d7f0bfad8
Thomas Lindop <thomas16256@gmail.com>
2024-10-21 14:19:27 +0100

    >> update aliases

--- .config/.bashrc.d/alias.sh
+++ .config/.bashrc.d/alias.sh
@@ -1,9 +1,7 @@
 alias ls="ls --color=always --group-directories-first --almost-all"
 alias lsn="ls --color=never"
-alias gist="git status"
 alias gitroot='cd $(git rev-parse --show-toplevel)'
 alias nv="nvim"
-alias nvf="nvim -c 'Fern .'"
 alias ff="clifm"
 alias py="python"
 alias code="codium"
@@ -11,6 +9,6 @@ alias tma="tmux attach || tmux"
 alias tmd="tmux detach"
 alias sbcl="rlwrap sbcl"
 alias shell="rlwrap xargs -L 1"
-alias fox="firefox --new-tab"
-alias yg='yona -s rg'
+alias yg='yona -s grep -Rn'
 alias ysh='yona -s'
+alias zathura='zathura --fork'
